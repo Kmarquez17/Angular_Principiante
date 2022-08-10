a39e77f0f83f15f3c79be1ee4dc5ee251d107d1f
Kevin Marquez <marquezkrodriguez24@gmail.com>
2022-08-09 23:11:10 -0600

    Creando modulo de rutas de la app

--- 03-paisesApp/src/app/app.module.ts
+++ 03-paisesApp/src/app/app.module.ts
@@ -4,16 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
 import { PaisModule } from './pais/pais.module';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    SharedModule,
-    PaisModule
-  ],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, PaisModule, SharedModule],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
