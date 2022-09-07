7d13714011e795639905e85f904647a9fa402489
Kevin Marquez <marquezkrodriguez24@gmail.com>
2022-09-06 20:53:42 -0600

    Terminando seccion 8 paises

--- 03-paisesApp/src/app/pais/pages/ver-pais/ver-pais.component.ts
+++ 03-paisesApp/src/app/pais/pages/ver-pais/ver-pais.component.ts
@@ -1,13 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { PaisService } from '../../services/pais.service';
-import { switchMap } from 'rxjs/operators';
+import { switchMap, tap } from 'rxjs/operators';
+import { Country } from '../../interfaces/pais.interface';
 
 @Component({
   selector: 'app-ver-pais',
   templateUrl: './ver-pais.component.html',
 })
 export class VerPaisComponent implements OnInit {
+  pais!: Country;
+
   constructor(
     private activaRoute: ActivatedRoute,
     private PaisService: PaisService
@@ -15,8 +18,11 @@ export class VerPaisComponent implements OnInit {
 
   ngOnInit(): void {
     this.activaRoute.params
-      .pipe(switchMap(({ id }) => this.PaisService.getPaisPorAlpha(id)))
-      .subscribe((resp) => {});
+      .pipe(
+        switchMap(({ id }) => this.PaisService.getPaisPorAlpha(id)),
+        tap()
+      )
+      .subscribe((pais) => (this.pais = pais));
     // this.activaRoute.params.subscribe(({ id }) => {
     //   this.PaisService.getPaisPorAlpha(id).subscribe((pais) => {
     //     console.log(pais);
